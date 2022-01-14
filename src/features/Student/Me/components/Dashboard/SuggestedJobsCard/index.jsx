ed97fcd595977b24f597e3cd32a0537ee04f6a55
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-01-15 00:51:07 +0700

    doing ui student dashboard

--- src/features/Student/Me/components/Dashboard/SuggestedJobsCard/index.jsx
+++ src/features/Student/Me/components/Dashboard/SuggestedJobsCard/index.jsx
@@ -1,10 +1,11 @@
+import Images from 'constants/images';
 import React from 'react';
+import * as BiIcons from 'react-icons/bi';
+import * as FaIcons from 'react-icons/fa';
+import * as MdIcons from 'react-icons/md';
+import * as RiIcons from 'react-icons/ri';
 // import PropTypes from 'prop-types';
 import './SuggestedJobsCard.scss';
-import * as BsIcons from 'react-icons/bs';
-import * as FaIcons from 'react-icons/fa';
-import * as BiIcons from 'react-icons/bi';
-import Images from 'constants/images';
 
 SuggestedJobsCard.propTypes = {
 
@@ -15,13 +16,13 @@ function SuggestedJobsCard(props) {
     <div className="suggested-jobs-card">
       <div className="suggested-jobs-card__date">
         <span>6 days ago</span>
-        <BsIcons.BsHeart className="suggested-jobs-card__date__icon-heart" />
+        <RiIcons.RiHeart3Line className="suggested-jobs-card__date__icon-heart" />
       </div>
       <div className="suggested-jobs-card__company-info">
         <div className="suggested-jobs-card__company-info__avatar">
           <img src={Images.tw} alt="company-avatar" />
           <div className="suggested-jobs-card__company-info__avatar__verify">
-            <BsIcons.BsFillCheckCircleFill
+            <FaIcons.FaCheckCircle
               className="suggested-jobs-card__company-info__avatar__verify__icon"
             />
           </div>
@@ -41,13 +42,23 @@ function SuggestedJobsCard(props) {
               <span>1000</span>
             </div>
           </div>
-          <div className="suggested-jobs-card__company-info__info__salary-location">
-            <div className="suggested-jobs-card__company-info__info__salary-location__salary">
-              <BiIcons.BiDollarCircle
-                className="suggested-jobs-card__company-info__info__salary-location__salary__icon"
-              />
-            </div>
-          </div>
+        </div>
+      </div>
+      <div className="suggested-jobs-card__salary-location">
+        <div className="suggested-jobs-card__salary-location__salary">
+          <BiIcons.BiDollarCircle
+            className="suggested-jobs-card__salary-location__salary__icon"
+          />
+          <span>$1000 - $2000</span>
+        </div>
+        <div className="suggested-jobs-card__salary-location__location">
+          <MdIcons.MdLocationOn
+            className="suggested-jobs-card__salary-location__location__icon"
+          />
+          <span>Da Nang, Viet Nam</span>
+        </div>
+        <div className="suggested-jobs-card__btn-apply">
+          <button className="btn btn-success btn-sm">Apply</button>
         </div>
       </div>
     </div>
