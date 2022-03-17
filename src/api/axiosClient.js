88ecac68c1aa51766ffe4e0c06832dc381954db3
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-03-17 19:00:47 +0700

    .

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -3,7 +3,7 @@ import queryString from 'query-string';
 
 
 const axiosClient = axios.create({
-  baseURL: process.env.REACT_APP_API_LOCAL_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'Content-Type': 'application/json',
     // Authorization: `Bearer ${token}`,
